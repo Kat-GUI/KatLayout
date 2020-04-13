56ebb3c8b265173f622d92b80f0bf77216e9c9ca
yoorkin <937697555@qq.com>
2020-04-13 16:36:57 +0800

    '修改文件结构,添加子模块'

--- test/CppKatLayoutTest.cpp
+++ test/CppKatLayoutTest.cpp
@@ -1,11 +1,32 @@
-﻿// CppKatLayoutTest.cpp : 定义应用程序的入口点。
-//
-
-#include "framework.h"
-#include "CppKatLayoutTest.h"
-#include "Layout.h"
+﻿#include<windows.h>
+#include"../src/Layout.h"
 #define MAX_LOADSTRING 100
+#define IDS_APP_TITLE			103
 
+#define IDR_MAINFRAME			128
+#define IDD_CPPKATLAYOUTTEST_DIALOG	102
+#define IDD_ABOUTBOX			103
+#define IDM_ABOUT				104
+#define IDM_EXIT				105
+#define IDI_CPPKATLAYOUTTEST			107
+#define IDI_SMALL				108
+#define IDC_CPPKATLAYOUTTEST			109
+#define IDC_MYICON				2
+#ifndef IDC_STATIC
+#define IDC_STATIC				-1
+#endif
+// 新对象的下一组默认值
+//
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+
+#define _APS_NO_MFC					130
+#define _APS_NEXT_RESOURCE_VALUE	129
+#define _APS_NEXT_COMMAND_VALUE		32771
+#define _APS_NEXT_CONTROL_VALUE		1000
+#define _APS_NEXT_SYMED_VALUE		110
+#endif
+#endif
 // 全局变量:
 HINSTANCE hInst;                                // 当前实例
 WCHAR szTitle[MAX_LOADSTRING];                  // 标题栏文本
